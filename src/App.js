22f165148bc45042282bf87c96cbbc02e9ded45c
jai sharma <jai@jungleegames.com>
2021-02-11 01:06:36 +0530

    Title component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Layout from './Containers/Layout/Layout';
 import Section from "./Containers/Section/Section";
 import Intro from './Components/Intro/Intro';
+import Title from "./Components/Title/Title";
 
 import './App.css';
 
@@ -12,6 +13,7 @@ class App extends Component {
                 <Layout>
                     <Section>
                         <Intro/>
+                            <Title>Web</Title>
                     </Section>
                 </Layout>
             </div>
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import classes from './Title.css';
+
+const title = props => (
+    <div className={classes.Title}>{props.children}</div>
+);
+
+export default title;
